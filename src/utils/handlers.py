47f6db769220022d7c93651ca9715d29e4fc9f53
AidanInceer <aidaninceer0@gmail.com>
2023-08-12 18:24:31 +0100

    update formatting

---
+++ src/utils/handlers.py
@@ -0,0 +1,17 @@
+from flask import Request
+
+
+def handle_request(request: Request):
+    envelope = request.get_json()
+    if not envelope:
+        msg = "no Pub/Sub message received"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    if not isinstance(envelope, dict) or "message" not in envelope:
+        msg = "invalid Pub/Sub message format"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    pubsub_message = envelope["message"]
+    return pubsub_message["data"]
